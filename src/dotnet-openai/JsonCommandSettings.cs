0590acb60e38eb7841ca3a475451b472338d1d7b
Daniel Cazzulino <daniel@cazzulino.com>
2025-04-15 11:16:46 -0300

    Add support for data annotations validation of command settings

--- src/dotnet-openai/JsonCommandSettings.cs
+++ src/dotnet-openai/JsonCommandSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using DataAnnotations = System.ComponentModel.DataAnnotations;
 
 namespace Devlooped.OpenAI;
 
@@ -24,6 +25,36 @@ public class JsonCommandSettings : CommandSettings
     {
         if (JQ.IsSet && !string.IsNullOrEmpty(JQ.Value))
             Json = true;
+
+        var context = new DataAnnotations.ValidationContext(this);
+        var results = new List<DataAnnotations.ValidationResult>();
+
+        if (!DataAnnotations.Validator.TryValidateObject(this, context, results, true))
+        {
+            var errorMessages = new List<string>();
+            foreach (var result in results)
+            {
+                if (result.MemberNames.Any() && result.ErrorMessage != null &&
+                    result.MemberNames.First() is { } memberName &&
+                    result.ErrorMessage.IndexOf(memberName) is var index &&
+                    index >= 0 &&
+                    GetType().GetProperty(memberName) is { } property &&
+                    property.GetCustomAttributesData()
+                            .FirstOrDefault(attr => attr.AttributeType == typeof(CommandArgumentAttribute) ||
+                                                    attr.AttributeType == typeof(CommandOptionAttribute))
+                            ?.ConstructorArguments
+                            .FirstOrDefault(arg => arg.ArgumentType == typeof(string))
+                            .Value?.ToString() is { } template)
+                {
+                    errorMessages.Add(template + ":" + result.ErrorMessage[(index + memberName.Length)..]);
+                }
+                else if (result.ErrorMessage != null)
+                {
+                    errorMessages.Add(result.ErrorMessage);
+                }
+            }
+            return ValidationResult.Error(string.Join("\n", errorMessages));
+        }
 
         return base.Validate();
     }
