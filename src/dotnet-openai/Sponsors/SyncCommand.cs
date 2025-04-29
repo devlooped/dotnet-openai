edcbe72a495a8c885d5593bfdc16ad0c103ec909
Daniel Cazzulino <daniel@cazzulino.com>
2025-04-29 14:31:25 -0300

    Fix DI for sponsor commands, refactor a bit
    The existing commands were not resolving anymore since we removed the ICommandApp from the DI container. This is because the lifetime of services gets all screwed up if you use Run() on the app more than once. So we instead execute the relevant commands directly.

---
+++ src/dotnet-openai/Sponsors/SyncCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Devlooped.Sponsors;
+using DotNetConfig;
+using Spectre.Console.Cli;
+
+namespace Devlooped.OpenAI.Sponsors;
+
+[Description("Synchronizes your sponsorship manifest for [lime]devlooped[/]")]
+class DevloopedSyncCommand(Config config, IGraphQueryClient client, IGitHubAppAuthenticator authenticator, IHttpClientFactory httpFactory)
+    : SyncCommand<DevloopedSyncCommand.DevloopedSyncSettings>(config, client, authenticator, httpFactory)
+{
+    public class DevloopedSyncSettings : SyncSettings, ISponsorableSettings
+    {
+        public string[]? Sponsorable { get; set; } = ["devlooped"];
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, DevloopedSyncSettings settings)
+    {
+        if (context.ShouldRunWelcome(config, settings))
+        {
+            if (new WelcomeCommand(config).Execute(context, new WelcomeCommand.WelcomeSettings { ToS = settings.ToS }) is var result && result != 0)
+                return result;
+        }
+
+        settings.Sponsorable = ["devlooped"];
+        return await base.ExecuteAsync(context, settings);
+    }
+}
---
+++ src/dotnet-openai/Sponsors/ViewCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Devlooped.Sponsors;
+using DotNetConfig;
+using Spectre.Console.Cli;
+
+namespace Devlooped.OpenAI.Sponsors;
+
+[Description("Validates and displays your sponsor manifest for [lime]devlooped[/], if present")]
+class DevloopedViewCommand(Config config, IHttpClientFactory http) : ViewCommand<DevloopedViewCommand.DevloopedViewSettings>(http)
+{
+    public class DevloopedViewSettings : ViewSettings, ISponsorableSettings
+    {
+        public string[]? Sponsorable { get; set; } = ["devlooped"];
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, DevloopedViewSettings settings)
+    {
+        if (context.ShouldRunWelcome(config, settings))
+        {
+            if (new WelcomeCommand(config).Execute(context, new WelcomeCommand.WelcomeSettings { ToS = settings.ToS }) is var result && result != 0)
+                return result;
+        }
+
+        settings.Sponsorable = ["devlooped"];
+        return await base.ExecuteAsync(context, settings);
+    }
+}
